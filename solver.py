ba4ccf55c78d9b013a7cfebc96bfac871c7b9511
unknown <sievala.a@gmail.com>
2020-03-20 00:16:05 +0200

    Removed main from solver, added testfile

--- solver.py
+++ solver.py
@@ -1,19 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from random import randint
-
-board = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9],
-]
-
 """
 Ratkaiseen pelilaudan `bo`
 takapakki (backtracking)
@@ -81,7 +67,3 @@ hyvän näköisenä konsoliin
 def printBoard(bo):
     for row in range(0, 9):
         print(bo[row])
-
-if __name__ == "__main__":
-    solve(board)
-    printBoard(board)
